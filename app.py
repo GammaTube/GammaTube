c6a569b59e0a5d23d3837f1c37194efb66f3aa25
Gamma7113131 <gametron2012@yahoo.com>
2024-11-06 21:54:24 +0000

    Update app.py

--- app.py
+++ app.py
@@ -452,6 +452,10 @@ def delete_playlist():
 
     return jsonify(success=True, message='Playlist deleted'), 200
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
     db.create_all()  # Create database tables
     app.run(debug=True)
