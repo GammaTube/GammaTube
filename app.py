21672c9a8b316abd5e414174d30287e98f21d472
Gamma7113131 <gametron2012@yahoo.com>
2024-12-12 20:32:40 +0000

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,6 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)  # Consistent usage of db.Column
-    permissions = db.Column(db.String(50), default='normal', nullable=False)  # Adding permissions column
 
     def __repr__(self):
         return f'<User {self.username}>'
